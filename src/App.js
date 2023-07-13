ff774d187f5092d414b6292a9cf43a02a2729ef0
heeyonhh <lightram23@gmail.com>
2023-07-13 15:04:07 +0900

    bootstrap img

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { Container, Nav, Navbar, Button, Row, Col } from 'react-bootstrap';
 import './App.css';
+import bg from './img/bg.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* classname으로 커스터마이징 가능 */}
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">EQL STORE</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg' style={{ backgroundImage : 'url('+ bg +')'}} />
+      
+      <Container>
+        <Row>
+          <Col>
+            <img src="./img/1.jpeg" width="70%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
