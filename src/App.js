023b41fdc08ea0467e27b48b25e2efd960ebd91b
heeyonhh <lightram23@gmail.com>
2023-07-13 16:26:10 +0900

    component map

--- src/App.js
+++ src/App.js
@@ -23,20 +23,27 @@ function App() {
       
       <Container>
         <Row>
-          <Card shoes={shoes[0]} i={1}/>
+          {/* <Card shoes={shoes[0]} i={1}/>
           <Card shoes={shoes[1]} i={2}/>
-          <Card shoes={shoes[2]} i={3}/>
+          <Card shoes={shoes[2]} i={3}/> */}
+          {
+            shoes.map(function(a, i){
+              return(
+                <Card shoes={shoes[i]} i={i}/>
+              )
+            })
+          }
+          {/* 어레이로 변경 */}
         </Row>
       </Container>
     </div>
   );
 }
 
-//props 문법으로 이미지 바꾸기 url 변경 되는 부분에 ''+ props.i +''
 function Card(props){
   return(
     <Col sm>
-      <img src={'https://heeyonhh.github.io/shop/src/img/'+ props.i +'.jpeg'} width="80%" />
+      <img src={'https://heeyonhh.github.io/shop/src/img/'+ (props.i+1) +'.jpeg'} width="80%" />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.content}</p>
       <p>{props.shoes.price}</p>
