e72c33200e1cf0044822fb104e0ef9a3343418a5
heeyonhh <lightram23@gmail.com>
2023-07-14 14:59:42 +0900

    routes route

--- src/App.js
+++ src/App.js
@@ -11,11 +11,6 @@ function App() {
   return (
     <div className="App">
 
-      <Routes>
-        <Route path="/detail" element={<div>상세페이지임</div>} />
-        <Route path="/about" element={<div>어바웃페이지임</div>} />
-      </Routes>
-
       <Navbar bg="light" data-bs-theme="light" className='nav'>
         <Container>
           <Navbar.Brand href="#home">EQL STORE</Navbar.Brand>
@@ -26,25 +21,33 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className='main-bg'/>
-      
-      <Container>
-        <Row>
-          { shoes.map(function(a, i){
-              return(
-                <Card shoes={shoes[i]} i={i} />
-              )
-            })}
-        </Row>
-      </Container>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className='main-bg' />
+            <Container>
+              <Row>
+                {shoes.map(function (a, i) {
+                  return (
+                    <Card shoes={shoes[i]} i={i} />
+                  )
+                })}
+              </Row>
+            </Container>
+          </>
+        } />
+        <Route path="/detail" element={<div>상세페이지임</div>} />
+        <Route path="/about" element={<div>어바웃페이지임</div>} />
+      </Routes>
+
     </div>
   );
 }
 
-function Card(props){
-  return(
+function Card(props) {
+  return (
     <Col sm>
-      <img src={'https://heeyonhh.github.io/shop/src/img/'+ (props.i+1) +'.jpeg'} width="80%" />
+      <img src={'https://heeyonhh.github.io/shop/src/img/' + (props.i + 1) + '.jpeg'} width="80%" />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.content}</p>
       <p>{props.shoes.price}</p>
