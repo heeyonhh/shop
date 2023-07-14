7d0e60664cee229efeeacb8c060cdca7b428c7f1
heeyonhh <lightram23@gmail.com>
2023-07-14 17:49:51 +0900

    useeffect 2

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -12,20 +12,17 @@ function Detail(props) {
     let [count, setCount ] = useState(0);
     let [alert, setAlert ] = useState(true);
 
+    useEffect(()=>{})
+    //재랜더링마다 코드 실행하고 싶으면
+    useEffect(()=>{},[])
+    //mount시 1회 코드 실행하고 싶으면
     useEffect(()=>{
-        let a = setTimeout(()=>{ setAlert(false) }, 2000)
-
         return()=>{
-            //  useEffect 전에 실행되고 싶을때 clean up function
-            // 기존 타이머는 제거해주세요
-            clearTimeout(a)
+            //unmount시 1회 코드 실행하고 싶으면
         }
-    }, [])
-    //[]useEffect 실행조건 넣을수 있는 곳 디펜던시/ count 가 업데이트 될때 / 1회만 실행되고 싶으면 [] 비어 놓기
-
-    //동적 ui만들기
-    //1.ui상태 저장할 state 만들고
-    //2.state에 따라서 ui가 어떻게 보일지 작성
+    })
+    useEffect(()=>{})
+    //{useEffect 실행 전에 실행 코드 안에 클린업 펑션}
 
     return (
         <>
