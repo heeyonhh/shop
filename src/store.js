bf96a7b23f74700676a0a40238827ea83d602b33
heeyonhh <lightram23@gmail.com>
2023-07-17 10:26:10 +0900

    redux

--- src/store.js
+++ src/store.js
@@ -1,7 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
 
+// redux : 컴포넌트간 state 공유 편해짐
+//usestate와 비슷함 slice라고 부름
+let user = createSlice({
+  name : 'user',
+  initialState : 'kim'
+})
+
+let stock = createSlice({
+  name : 'stock',
+  initialState : [10,11,12]
+})
+
+//등록
 export default configureStore({
-  reducer: { }
+  reducer: {
+    user : user.reducer,
+    stock : stock.reducer
+  } 
 }) 
-
-//redux 는 리액트 버전 18.1 이상이여야함. store 파일 만들어서 위에 데이터 import 후 index에 provider 감싸기
